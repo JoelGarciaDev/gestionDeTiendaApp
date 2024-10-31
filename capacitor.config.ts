dc8eecda5772f6dffcafb34a10b968162a56dd43
joel_garcia <jgarcia211@ucol.mx>
2024-10-31 12:18:43 -0600

    Tienda App

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'lista5fx',
+  webDir: 'www'
+};
+
+export default config;
